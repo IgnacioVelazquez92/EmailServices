08ec64dea73e115a88e79e47d920abb9a1747fae
Ignacio Velazquez <ignaciov92@gmail.com>
2025-01-06 14:16:53 -0300

    se guardan los estilos de los formularios de ingreso y registro

--- Contacts/forms.py
+++ Contacts/forms.py
@@ -1,23 +1,61 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True, label="Correo Electrónico")
-    first_name = forms.CharField(required=True, label="Nombre")
-    last_name = forms.CharField(required=True, label="Apellido")
+    email = forms.EmailField(
+        required=True,
+        label="Correo Electrónico",
+        widget=forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Ingresa tu correo electrónico'})
+    )
+    first_name = forms.CharField(
+        required=True,
+        label="Nombre",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Ingresa tu nombre'})
+    )
+    last_name = forms.CharField(
+        required=True,
+        label="Apellido",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Ingresa tu apellido'})
+    )
+    username = forms.CharField(
+        label="Nombre de usuario",
+        widget=forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Crea tu nombre de usuario'})
+    )
+    password1 = forms.CharField(
+        label="Contraseña",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Crea una contraseña segura'})
+    )
+    password2 = forms.CharField(
+        label="Confirmar contraseña",
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Confirma tu contraseña'})
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name',
                   'last_name', 'password1', 'password2']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        if commit:
-            user.save()
-        return user
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ingresa tu usuario',
+        }),
+        label="Usuario"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Ingresa tu contraseña',
+        }),
+        label="Contraseña"
+    )
--- Contacts/views.py
+++ Contacts/views.py
@@ -3,29 +3,31 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 
 def registro(request):
     if request.method == 'GET':
-        return render(request, 'session/register.html', {'form': UserCreationForm})
+        return render(request, 'session/register.html', {'form': CustomUserCreationForm()})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(
-                    request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('index')
-            except:
-                return render(request, 'session/register.html', {
-                    'form': UserCreationForm,
-                    'mensaje': 'Usuario ya existe'
-                })
-
-        return render(request, 'session/register.html', {
-            'form': UserCreationForm,
-            'mensaje': 'Las contraseñas no coinciden'
-        })
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():  # Validar el formulario automáticamente
+            # Guardar el usuario sin persistirlo aún
+            user = form.save(commit=False)
+            user.username = form.cleaned_data['username']
+            user.email = form.cleaned_data['email']
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.save()  # Persistir en la base de datos
+            login(request, user)  # Autenticar y loguear al usuario
+            # Redirigir al usuario al dashboard o página principal
+            return redirect('index')
+        else:
+            # Si el formulario no es válido, mostrar los errores
+            return render(request, 'session/register.html', {
+                'form': form,
+                'mensaje': 'Por favor, corrige los errores en el formulario.'
+            })
 
 
 def index(request):
@@ -37,20 +39,44 @@ def cerrar_sesion(request):
     return redirect('index')
 
 
+# def iniciar_sesion(request):
+#     if request.method == 'GET':
+#         return render(request, 'session/signin.html', {
+#             'form': AuthenticationForm(),
+#         })
+#     else:
+#         # Pasar datos directamente al formulario
+#         form = AuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, username=username, password=password)
+
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('index')
+#         # Si el formulario no es válido o la autenticación falla
+#         return render(request, 'session/signin.html', {
+#             'form': form,
+#             'mensaje': 'Usuario o contraseña incorrectos'
+#         })
+
 def iniciar_sesion(request):
     if request.method == 'GET':
         return render(request, 'session/signin.html', {
-            'form': AuthenticationForm,
+            'form': CustomAuthenticationForm(),  # Usamos el formulario personalizado
         })
     else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
+        form = CustomAuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        if user is None:
-            return render(request, 'session/signin.html', {
-                'form': AuthenticationForm,
-                'mensaje': 'Usuario o contraseña incorrectos'
-            })
-        else:
-            login(request, user)
-            return redirect('index')
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+        return render(request, 'session/signin.html', {
+            'form': form,
+            'mensaje': 'Usuario o contraseña incorrectos'
+        })
